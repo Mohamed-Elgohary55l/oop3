bb92159ff90bb0fe33950c68090d69130b44abec
Mohamed-Elgohary55l <mohamedelgohary476@gmail.com>
2026-09-04 23:10:50 +0300

    Q1 Overloading, Overriding, and Binding a) What is the difference between Method Overloading and Method Overriding? b) What is the difference between Static Binding and Dynamic Binding?

--- oop3/Program.cs
+++ oop3/Program.cs
@@ -6,7 +6,17 @@
         {
 
             #region Q1 Overloading, Overriding, and Bindinga) What is the difference between Method Overloading and Method Overriding? b) What is the difference between Static Binding and Dynamic Binding?
+            /*
+           a) Method Overloading is a feature that allows a class to have multiple methods with the same
+          name but different parameter (different type, number, or order of parameters).
+         overriding is a feature that allows a subclass to provide a specific implementation of a method that is already defined in the parent class. 
+            The method in the subclass must have the same name, return type, and parameters as the method in the superclass.
 
+
+    b) static binding is a process where the method to be called at compile time.
+          It is associated with method overloading, where the compiler determines which method to call based on the method signature.
+        dynamic binding is a process where the method to be called is determined at runtime by clr.
+                */
             #endregion
 
             /******************************************************/
