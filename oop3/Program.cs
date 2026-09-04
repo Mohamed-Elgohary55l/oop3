9360a122476a39c65fd36851f81bea83b67240b5
Mohamed-Elgohary55l <mohamedelgohary476@gmail.com>
2026-09-04 23:03:46 +0300

    Add project files.

---
+++ oop3/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace oop3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Q1 Overloading, Overriding, and Bindinga) What is the difference between Method Overloading and Method Overriding? b) What is the difference between Static Binding and Dynamic Binding?
+
+            #endregion
+
+            /******************************************************/
+
+
+            #region q
+
+            #endregion
+
+            /******************************************************/
+
+
+            #region q
+
+            #endregion
+
+            /******************************************************/
+
+
+            #region q
+
+            #endregion
+
+            /******************************************************/
+
+
+            #region q
+
+            #endregion
+
+            /******************************************************/
+
+
+            #region q
+
+            #endregion
+
+            /******************************************************/
+
+
+
+        }
+    }
+}
